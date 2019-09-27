cebc9a93656feeb39505f43dd6134c5a1d49a48b
Read <decimation001@gmail.com>
2019-09-27 13:59:22 -0500

    Initial commit

---
+++ Bromine/ChromeDriver.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using OpenQA.Selenium.Chrome;
+
+namespace Bromine
+{
+	/// <summary>
+	/// Provides utilities for <see cref="ChromeDriver"/>
+	/// </summary>
+	public static class ChromeUtil
+	{
+		public const string NAME = "chromedriver";
+
+		public static ChromeDriver CreateQuietHeadless(params string[] extraArgs)
+		{
+			var list = extraArgs.ToList();
+			list.Add("--headless");
+
+			return CreateQuiet(list.ToArray());
+		}
+
+		public static ChromeDriver CreateQuiet(params string[] extraArgs)
+		{
+			var co = new ChromeOptions();
+
+			co.AddArguments("--silent", "--log-level=3", "--disable-infobars");
+			co.AddArguments("--mute-audio", "--disable-gpu");
+//			co.AddArguments("--disable-extensions","start-maximized");
+
+			if (extraArgs?.Length > 0)
+				co.AddArguments(extraArgs);
+
+			// hide logs
+			var service = ChromeDriverService.CreateDefaultService();
+			service.HideCommandPromptWindow = true;
+
+			return new ChromeDriver(service, co);
+		}
+	}
+}
